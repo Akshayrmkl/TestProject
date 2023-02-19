ef8760abf9a42e9d73ddc9443e3493dd95cfd58b
akshay <akshayrmkl@gmail.com>
2023-02-19 14:26:53 +0530

    initial

---
+++ Python learning2/SmallProj/SmallProj/untitled0.py
@@ -0,0 +1,10 @@
+
+# ---------------------------
+#CLASS
+# ---------------------------
+class MyClass:
+    variable = "blah"
+    def function(self):
+        print("This is a message inside the class.")        
+myobjectx = MyClass()
+myobjectx.function()
